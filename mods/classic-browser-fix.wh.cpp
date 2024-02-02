351dfed6c96fd7506922fdd7abede44b8e65fcd5
Anixx <neptunia@mail.ru>
2024-02-02 10:10:44 +0200

    Update classic-browser-fix.wh.cpp
    Remove UWP for now, not all need this fix

--- mods/classic-browser-fix.wh.cpp
+++ mods/classic-browser-fix.wh.cpp
@@ -3,21 +3,20 @@
 // @name            Fix browsers for Windows Classic theme
 // @description     Forces the border from Aero Light theme to fix glitched border in Classic theme
 // @version         1.0
-// @author 			    Anixx
-// @github 			    https://github.com/Anixx
+// @author 			Anixx
+// @github 			https://github.com/Anixx
 // @include         msedge.exe
 // @include         chrome.exe
 // @include         chromium.exe
 // @include         steam.exe
-// @include         ApplicationFrameHost.exe
+
 // @compilerOptions -luxtheme
 // ==/WindhawkMod==
 
 // ==WindhawkModReadme==
 /*
-This mod forces Aero Lite window borders on browsers MS Edge, Chrome, Chromium, Steam app and UWP apps,
-which fixes their glitches when using Windows Classic theme. Note that UWP apps need a separate fix
-to be able to run under Windows Classic theme (https://winclassic.net/thread/2058/classic-uwp-fix).
+This mod forces Aero Lite window borders on browsers MS Edge, Chrome, Chromium and Steam app,
+which fixes their glitches when using Windows Classic theme.
 
 ![Aero Lite](https://i.imgur.com/5enqSD8.png)
 
